14b108684ab0c1ea51701cdd1f89099f19bc4e76
Tyler-Phillips12 <Tyler-Phillips12@github.com>
2023-07-30 21:26:02 -0400

    added back og code

---
+++ build/src/components/Footer/footer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import './footer.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faGithub,
+    faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+
+
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <h3> Links </h3>
+            <a href="https://github.com/Tyler-Phillips12" 
+                className= 'github'>
+                    <FontAwesomeIcon icon={faGithub} size="2x" />
+                </a>
+                <a href="https://www.linkedin.com/in/tyler-phillips-63651677/"
+                className= 'linkedin'>
+                <FontAwesomeIcon icon={faLinkedin} size="2x" />
+                </a>
+        </footer>
+    );
+}
---
+++ build/src/components/Header/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './header.css'
+import Navigation from '../Navigation/navigation';
+
+export default function Header() {
+    return (
+    <header className="header">
+      <div >
+        <h1> Welcome to my Portfolio! </h1>
+      </div>
+    </header>
+    );
+}
